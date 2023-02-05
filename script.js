8f5bbd9d27347cfd76251bab707d5676aa304002
Kelvin Morais <kelvintitomorais@hotmail.com>
2023-02-05 01:53:17 -0300

    teste

--- script.js
+++ script.js
@@ -1,21 +1,51 @@
-var datanasci = window.document.querySelector('input#nasci')
-var sexom = window.document.querySelector('input#m')
-var sexof = window.document.querySelector('input#f')
 var botao = window.document.querySelector('input#verificar')
-var frase = window.document.querySelector('div#div-idade')
-var foto = window.document.querySelector('div#foto')
-var div = window.document.querySelector('div#div-idade')
-
-var verificar = are.addEventListener('click', botao)
-
-window.document.alert("teste")
-frase.innerHTML = "teste"
+botao.addEventListener('click', verificar)
 
 
+function verificar(){
+    var data = new Date(),
+        dia  = data.getDate().toString(),
+        diaF = (dia.length == 1) ? '0'+dia : dia,
+        mes  = (data.getMonth()+1).toString(), //+1 pois no getMonth Janeiro começa com zero.
+        mesF = (mes.length == 1) ? '0'+mes : mes,
+        anoF = data.getFullYear();
+    return diaF+"/"+mesF+"/"+anoF;
+}
 
-window.document.write("teste")
-window.document.write(datanasci)
-window.document.write(datanasci)
-window.document.write(datanasci)
 
+function verificar(){
+    var data = new Date()
 
+    var dia = data.getDate()
+    var mes = data.getUTCMonth()
+    mes = mes+1
+    var ano = data.getFullYear()
+    
+    var fano = window.document.querySelector('input#txtano')
+    var res = window.document.querySelector('div#res')
+    window.document.write(fano)
+    
+    if (fano.value.length == 0 || Number(fano.value) > ano){
+        window.alert('[ERRO] Verifique os dados e tente novamente!')
+    } else{
+        window.alert('tudo ok')
+    }
+}
+    
+   // window.document.write("teste")
+    
+      
+      
+    
+   // window.document.write("teste1")
+       
+    
+   // window.document.write("teste2")
+    
+    
+    //window.document.write(dia)
+    //window.document.write(mes)
+    //window.document.write(ano)
+
+    //window.alert('teste')
+    //window.document.write("teste")
