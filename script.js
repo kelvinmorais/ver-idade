578040ce37b24452b7c77c6f5aec813a6f1ac7e4
Kelvin Morais <kelvintitomorais@hotmail.com>
2023-02-05 00:20:12 -0300

    ok

---
+++ script.js
@@ -0,0 +1,21 @@
+var datanasci = window.document.querySelector('input#nasci')
+var sexom = window.document.querySelector('input#m')
+var sexof = window.document.querySelector('input#f')
+var botao = window.document.querySelector('input#verificar')
+var frase = window.document.querySelector('div#div-idade')
+var foto = window.document.querySelector('div#foto')
+var div = window.document.querySelector('div#div-idade')
+
+var verificar = are.addEventListener('click', botao)
+
+window.document.alert("teste")
+frase.innerHTML = "teste"
+
+
+
+window.document.write("teste")
+window.document.write(datanasci)
+window.document.write(datanasci)
+window.document.write(datanasci)
+
+
