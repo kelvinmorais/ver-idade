142baa13a111acff036afa89b73a8d0623796810
Kelvin Morais <kelvintitomorais@hotmail.com>
2023-02-06 01:35:58 -0300

    ok

--- script.js
+++ script.js
@@ -47,97 +47,97 @@ function verificar(){   //função ao button ser pressionado
             if (anoidade >= 0 && anoidade < 3 && genero == 'Homem') {  //HOME
 
                 img.setAttribute('src','img/1-bebe-m-200.png') //add a imagem 
-                res.innerHTML = `Você é um bebê com ${qtdanos.toFixed(1)} anos de idade, que são ${qtdmeses.toFixed(0)} meses ou ${diastotal} dias de vida. <br>`
+                res.innerHTML = `Você é um bebê com ${qtdanos.toFixed(1)} anos de idade, ao total ${qtdmeses.toFixed(0)} meses ou ${diastotal} dias de vida. <br>`
                 res.appendChild(img) //add imagem no html
 
             } else if (anoidade >= 3 && anoidade < 6 && genero == 'Homem'){
 
                 img.setAttribute('src','img/2-crianca-m-200.png')
-                res.innerHTML = `Você é uma criança com ${qtdanos.toFixed(1)} anos de idade, que são ${qtdmeses.toFixed(0)} meses ou ${diastotal} dias de vida. <br>`
+                res.innerHTML = `Você é uma criança com ${qtdanos.toFixed(1)} anos de idade, ao total ${qtdmeses.toFixed(0)} meses ou ${diastotal} dias de vida. <br>`
                 res.appendChild(img) 
 
             } else if (anoidade >= 6 && anoidade < 11 && genero == 'Homem'){
 
                 img.setAttribute('src','img/3-menino-m-200.png')
-                res.innerHTML = `Você é um menino com ${qtdanos.toFixed(1)} anos de idade, que são ${qtdmeses.toFixed(0)} meses ou ${diastotal} dias de vida. <br>`
+                res.innerHTML = `Você é um menino com ${qtdanos.toFixed(1)} anos de idade, ao total ${qtdmeses.toFixed(0)} meses ou ${diastotal} dias de vida. <br>`
                 res.appendChild(img) 
 
             } else if (anoidade >= 11 && anoidade < 16 && genero == 'Homem'){
 
                 img.setAttribute('src','img/4-pre-adolescente-m-200.png')
-                res.innerHTML = `Você é um pré adolescente com ${qtdanos.toFixed(1)} anos de idade, que são ${qtdmeses.toFixed(0)} meses ou ${diastotal} dias de vida. <br>`
+                res.innerHTML = `Você é um pré adolescente com ${qtdanos.toFixed(1)} anos de idade, ao total ${qtdmeses.toFixed(0)} meses ou ${diastotal} dias de vida. <br>`
                 res.appendChild(img) 
 
             } else if (anoidade >= 16 && anoidade < 19 && genero == 'Homem'){
 
                 img.setAttribute('src','img/5-adolescente-m-200.png')
-                res.innerHTML = `Você é um adolescente com ${qtdanos.toFixed(1)} anos de idade, que são ${qtdmeses.toFixed(0)} meses ou ${diastotal} dias de vida. <br>`
+                res.innerHTML = `Você é um adolescente com ${qtdanos.toFixed(1)} anos de idade, ao total ${qtdmeses.toFixed(0)} meses ou ${diastotal} dias de vida. <br>`
                 res.appendChild(img) 
 
             } else if (anoidade >= 19 && anoidade < 31 && genero == 'Homem'){
 
                 img.setAttribute('src','img/6-adulto-m-200.png')
-                res.innerHTML = `Você é um Adulto com ${qtdanos.toFixed(1)} anos de idade, que são ${qtdmeses.toFixed(0)} meses ou ${diastotal} dias de vida. <br>`
+                res.innerHTML = `Você é um Adulto com ${qtdanos.toFixed(1)} anos de idade, ao total ${qtdmeses.toFixed(0)} meses ou ${diastotal} dias de vida. <br>`
                 res.appendChild(img)
 
             } else if (anoidade >= 31 && anoidade < 61 && genero == 'Homem'){
 
                 img.setAttribute('src','img/7-home-experiente-m-200.png')
-                res.innerHTML = `Você é um homem experiente com ${qtdanos.toFixed(1)} anos de idade, que são ${qtdmeses.toFixed(0)} meses ou ${diastotal} dias de vida. <br>`
+                res.innerHTML = `Você é um homem experiente com ${qtdanos.toFixed(1)} anos de idade, ao total ${qtdmeses.toFixed(0)} meses ou ${diastotal} dias de vida. <br>`
                 res.appendChild(img) 
 
             } else if (anoidade >= 61 && genero == 'Homem'){
 
                 img.setAttribute('src','img/8-idoso-m-200.png')
-                res.innerHTML = `Você é um idoso com ${qtdanos.toFixed(1)} anos de idade, que são ${qtdmeses.toFixed(0)} meses ou ${diastotal} dias de vida. <br>`  
+                res.innerHTML = `Você é um idoso com ${qtdanos.toFixed(1)} anos de idade, ao total ${qtdmeses.toFixed(0)} meses ou ${diastotal} dias de vida. <br>`  
                 res.appendChild(img) 
 
             } else if (anoidade >= 0 && anoidade < 3 && genero == 'Mulher') { //MULHER
 
                 img.setAttribute('src','img/11-bebe-f-200.png')
-                res.innerHTML = `Você é uma bebê com ${qtdanos.toFixed(1)} anos de idade, que são ${qtdmeses.toFixed(0)} meses ou ${diastotal} dias de vida. <br>`
+                res.innerHTML = `Você é uma bebê com ${qtdanos.toFixed(1)} anos de idade, ao total ${qtdmeses.toFixed(0)} meses ou ${diastotal} dias de vida. <br>`
                 res.appendChild(img)
 
             } else if (anoidade >= 3 && anoidade < 6 && genero == 'Mulher'){
 
                 img.setAttribute('src','img/12-crianca-f-200.png')
-                res.innerHTML = `Você é uma criança com ${qtdanos.toFixed(1)} anos de idade, que são ${qtdmeses.toFixed(0)} meses ou ${diastotal} dias de vida. <br>`
+                res.innerHTML = `Você é uma criança com ${qtdanos.toFixed(1)} anos de idade, ao total ${qtdmeses.toFixed(0)} meses ou ${diastotal} dias de vida. <br>`
                 res.appendChild(img)
 
             } else if (anoidade >= 6 && anoidade < 11 && genero == 'Mulher'){
 
                 img.setAttribute('src','img/13-menina-f-200.png')
-                res.innerHTML = `Você é uma menina com ${qtdanos.toFixed(1)} anos de idade, que são ${qtdmeses.toFixed(0)} meses ou ${diastotal} dias de vida. <br>`
+                res.innerHTML = `Você é uma menina com ${qtdanos.toFixed(1)} anos de idade, ao total ${qtdmeses.toFixed(0)} meses ou ${diastotal} dias de vida. <br>`
                 res.appendChild(img)
 
             } else if (anoidade >= 11 && anoidade < 16 && genero == 'Mulher'){
 
                 img.setAttribute('src','img/15-pre-adolescente-f-200.png')
-                res.innerHTML = `Você é uma pré adolescente com ${qtdanos.toFixed(1)} anos de idade, que são ${qtdmeses.toFixed(0)} meses ou ${diastotal} dias de vida. <br>`
+                res.innerHTML = `Você é uma pré adolescente com ${qtdanos.toFixed(1)} anos de idade, ao total ${qtdmeses.toFixed(0)} meses ou ${diastotal} dias de vida. <br>`
                 res.appendChild(img)
 
             } else if (anoidade >= 16 && anoidade < 19 && genero == 'Mulher'){
                 
                 img.setAttribute('src','img/16-adolescente-f-200.png')
-                res.innerHTML = `Você é uma adolescente com ${qtdanos.toFixed(1)} anos de idade, que são ${qtdmeses.toFixed(0)} meses ou ${diastotal} dias de vida. <br>`
+                res.innerHTML = `Você é uma adolescente com ${qtdanos.toFixed(1)} anos de idade, ao total ${qtdmeses.toFixed(0)} meses ou ${diastotal} dias de vida. <br>`
                 res.appendChild(img)
 
             } else if (anoidade >= 19 && anoidade < 31 && genero == 'Mulher'){
 
                 img.setAttribute('src','img/17-adulta-f-200.png')
-                res.innerHTML = `Você é uma adulta com ${qtdanos.toFixed(1)} anos de idade, que são ${qtdmeses.toFixed(0)} meses ou ${diastotal} dias de vida.<br>`
+                res.innerHTML = `Você é uma adulta com ${qtdanos.toFixed(1)} anos de idade, ao total ${qtdmeses.toFixed(0)} meses ou ${diastotal} dias de vida.<br>`
                 res.appendChild(img)
 
             } else if (anoidade >= 31 && anoidade < 61 && genero == 'Mulher'){
 
                 img.setAttribute('src','img/18-mulher-experiente-f-200.png')
-                res.innerHTML = `Você é uma mulher experiente com ${qtdanos.toFixed(1)} anos de idade, que são ${qtdmeses.toFixed(0)} meses ou ${diastotal} dias de vida.<br>`
+                res.innerHTML = `Você é uma mulher experiente com ${qtdanos.toFixed(1)} anos de idade, ao total ${qtdmeses.toFixed(0)} meses ou ${diastotal} dias de vida.<br>`
                 res.appendChild(img)
 
             } else if (anoidade >= 61 && genero == 'Mulher'){
 
                 img.setAttribute('src','img/18-idoso-f-200.png')
-                res.innerHTML = `Você é uma idosa com ${qtdanos.toFixed(1)} anos de idade, que são ${qtdmeses.toFixed(0)} meses ou ${diastotal} dias de vida.<br>` 
+                res.innerHTML = `Você é uma idosa com ${qtdanos.toFixed(1)} anos de idade, ao total ${qtdmeses.toFixed(0)} meses ou ${diastotal} dias de vida.<br>` 
                 res.appendChild(img) 
             }
 
